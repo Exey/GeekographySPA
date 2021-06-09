8e60e7fe92ba3469c709694db5f3184ef2b93a5b
Exey Panteleev <exeypanteleev@gmail.com>
2021-06-10 01:17:46 +0300

    Initial
    Learning Svelte by Tutorials

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let name: string;
+	import InactionCard from "./comps/InactionCard.svelte"
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p class="intro">To do:</p>
+	<InactionCard
+		title="Learn Svelte and TypeScript"
+		notes="Share my website as open-source to github"
+	/>
+	<InactionCard
+		title="Learn Svelte and TypeScript"
+		notes="Share my website as open-source to github"
+	/>
+	<InactionCard
+		title="Learn Svelte and TypeScript"
+		notes="Share my website as open-source to github"
+	/>
+		
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/comps/InactionCard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let title, notes;
+</script>
+
+<div class="inaction">
+    <h2 class="title">{title}</h2>
+    <div class="notes">{notes}</div>
+</div>
